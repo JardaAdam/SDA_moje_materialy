7abbfaffff4bae3a8780dfeb3bdb5fb43d31c9a5
JardaAdam <jardys23@gmail.com>
2024-07-07 00:25:46 +0200

    made task snake

--- exceptions/tasks/uncontrollable_snake.py
+++ exceptions/tasks/uncontrollable_snake.py
@@ -5,4 +5,20 @@
 # Je vhodne pouzit napriklad `time.sleep(1)` (`import time`), aby program cakal 1 sekundu.
 
 # Ako potom program vypnete?
+"""vypnu dalsim stisknutim stejne kombinace klavesnic (u me ctrl+F2)"""
 # Ostava ako uloha pre pozornych citatelov
+
+
+import time
+
+
+"""while True is superior so it will keep repeating itself"""
+i = 0
+while True:
+    try:
+        print(f'Puthon {i} initiated...')
+        i += 1
+        time.sleep(2)
+    except KeyboardInterrupt:
+        print('You cannot stop me that easily!')
+
