742bc44342284f60de942843b76949f0f9deff45
JardaAdam <jardys23@gmail.com>
2024-07-06 19:21:21 +0200

    crated new file and moved Abstract part

--- moje_prace_komenty/person.py
+++ moje_prace_komenty/person.py
@@ -70,7 +70,7 @@ class Student(Person):
 
 class WorkingStudent(Employee, Student):
     """muzu zadat za sebe dedicnost pro vice trid naraz
-    lepsi pouzivat pr mixin nez pro dijamant"""
+    lepsi pouzivat pr mixin nez pro diamant"""
 
     def __init__(self, name, age, rate, num_of_hour, scholarship):
         Employee.__init__(self, name, age, rate, num_of_hour)
@@ -101,57 +101,3 @@ def check_finance(obj):
 check_finance(os2)  # an instance of the Employee class
 check_finance(os3)  # an instance of the Student class
 
-"""Abstraktní třída je zobecněním jiných tříd, ale sama o sobě neexistuje (nelze vytvářet objekty tohoto typu). 
-Například třídy jako Kruh, Čtverec a Trojúhelník odpovídají skutečným geometrickým útvarům a každá z nich 
-má specifické vlastnosti. Můžeme je však všechny zobecnit na třídu Utvar, která ale přímo neexistuje a 
-nemá ani žádné objekty - bude to tedy abstraktní třída s metodami, které jsou společné všem těmto útvárům."""
-
-"""K vytvoření abstraktních tříd použijeme balíček abc. Abstraktní metody nemají tělo a musí být 
-implementovány dědičnými metodami."""
-
-from abc import ABC, abstractmethod
-from math import pi
-
-
-class Figure(ABC):
-    @abstractmethod
-    def circuit(self):
-        pass
-
-    @abstractmethod
-    def area(self):
-        pass
-
-
-"""Třídy rozšiřující třídu Utvar."""
-
-
-class Rectangle(Figure):
-    def __init__(self, a, b):
-        self.a = a
-        self.b = b
-
-    def circuit(self):
-        return 2 * (self.a + self.b)
-
-    def area(self):
-        return self.a * self.b
-
-
-class Circle(Figure):
-    def __init__(self, r):
-        self.r = r
-
-    def circuit(self):
-        return 2 * self.r * pi
-
-    def area(self):
-        return pi * self.r ** 2
-
-
-"""volani"""
-
-rectangle = Rectangle(3, 5)
-circle = Circle(12)
-print(rectangle.circuit())
-print(circle.circuit())
