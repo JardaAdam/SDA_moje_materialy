1012313fb4e1751b89d015670e426cf8b0c590d4
JardaAdam <jardys23@gmail.com>
2024-07-07 00:05:16 +0200

    changes in file.txt

--- exceptions/tasks/file_permissions.py
+++ exceptions/tasks/file_permissions.py
@@ -4,7 +4,23 @@
 # Pre windows userov, ako nastavit, aby bol subor readonly:
 # https://adamtheautomator.com/how-to-make-a-file-read-only/
 
-with open("file_readonly.txt", "w") as new_file:
-    new_file.writelines("learn how to create folder readonly")
+# with open("text_file.txt", "a") as file:
+#     file.write("\nthis line write by append")
+#
+#
+# with open("readonly_file.txt", "a") as new_file:
+#     new_file.writelines("\nI want to write here")
 
-    
+def append_text(filename: str, text: str) -> None:
+    try:
+        with open(filename, 'a') as file:
+            """ 'a' = append """
+            print(text, file=file)
+            """in print we writen to our file"""
+    except PermissionError:
+        print(f"Cannot write to this file {filename}")
+
+
+if __name__ == "__main__":
+    append_text('text_file.txt', '\nadd some words to this file')
+    append_text('readonly_file.txt', 'I learn how do exceptions')
