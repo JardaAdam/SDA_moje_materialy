72a2bc730baab640200997ce7a3bb19b11c1e790
JardaAdam <jardys23@gmail.com>
2024-07-16 20:57:07 +0200

    uprava v Http.py

--- moje_prace_komenty/Http.py
+++ moje_prace_komenty/Http.py
@@ -4,17 +4,18 @@ class NotFoundExeption(Exception):
     pass
 
 
-def get_residents_count(planet_url):
+def get_residents(planet_url):
     response = requests.get(planet_url)
     if response.status_code == 404:
             raise NotFoundExeption("Planet not found")
 
     data = response.json()
-    return len(data['residents'])
+
+    return (data['residents'])
 
 
 url = 'http://swapi.dev/api/planets/1/'
-print(get_residents_count(url))
+print(get_residents(url))
 
 
 
